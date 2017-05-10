8d9fe2168f06881928b29fb44cc29af27a5f55fb
ZanderKirilov <biross11@gmail.com>
2017-05-10 18:17:49 +0300

    Commit answer

--- 05. Invalid Number/Program.java
+++ 05. Invalid Number/Program.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class InvalidNumber {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		double num = Double.parseDouble(console.nextLine());
+		
+		boolean inRange = (num >= 100 && num <= 200) || num == 0;
+		if (!inRange)
+		  System.out.println("invalid");
+
+
+	}
+
+}
 
