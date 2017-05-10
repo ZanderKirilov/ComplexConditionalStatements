3082667cbc91a5421e22b3c4f7ff64907839bbe9
ZanderKirilov <biross11@gmail.com>
2017-05-10 21:18:46 +0300

    Create Program.java

---
+++ 08. Trade Comissions/Program.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class TradeComissions {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		String town = console.nextLine().toLowerCase();
+		double sales = Double.parseDouble(console.nextLine());
+		double commission = -1.0;
+		
+		if (town.equals("sofia")) {
+			if (sales >= 0 && sales <= 500) commission = 0.05;
+			else if (sales > 500 && sales <= 1000) commission = 0.07;
+			else if (sales > 1000 && sales <= 10000) commission = 0.08;
+			else if (sales > 10000) commission = 0.12;
+			} else if (town.equals("varna")) {
+				if (sales >= 0 && sales <= 500) commission = 0.045;
+				else if (sales > 500 && sales <= 1000) commission = 0.075;
+				else if (sales > 1000 && sales <= 10000) commission = 0.10;
+				else if (sales > 10000) commission = 0.13;
+			} else if (town.equals("plovdiv")) {
+				if (sales >= 0 && sales <= 500) commission = 0.055;
+				else if (sales > 500 && sales <= 1000) commission = 0.08;
+				else if (sales > 1000 && sales <= 10000) commission = 0.12;
+				else if (sales > 10000) commission = 0.145;
+			} else {
+				System.out.println("error");
+			}
+		if (commission >0){
+		System.out.printf("%.2f", commission * sales);
+		} else {
+			System.out.println("error");
+		}
+		}
+
+	}
