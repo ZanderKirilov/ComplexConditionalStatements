2565ba06307a12a6592f647b7fe929e4c8c71108
ZanderKirilov <biross11@gmail.com>
2017-05-10 18:13:55 +0300

    Commit answer

--- 04. Fruit or Vegetable/Program.java
+++ 04. Fruit or Vegetable/Program.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class FruitOrVegetable {
+
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		
+		String entrance = console.nextLine();
+		
+		if (entrance.equals("banana") || entrance.equals("apple") || entrance.equals("kiwi") || entrance.equals("cherry") || entrance.equals("lemon") || entrance.equals("grapes")) {
+			System.out.println("fruit");
+		} else if (entrance.equals("tomato") || entrance.equals("cucumber") || entrance.equals("pepper") || entrance.equals("carrot")) {
+			System.out.println("vegetable");
+		}else {
+			System.out.println("unknown");
+		}
+	}
+
+}
